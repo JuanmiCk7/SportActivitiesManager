50ff5e299732bdf087604de60c06a232ab071a28
JuanmiCk7 <juanmicabotsmr@gmail.com>
2022-07-29 16:08:55 +0200

    First commit...

---
+++ src/main/java/com/juanmi/spring/SportActivitiesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.juanmi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportActivitiesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportActivitiesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanmi/spring/SportActivitiesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanmi.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportActivitiesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
